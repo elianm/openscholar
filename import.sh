d78ee2d0803eb2500a588a42d32f3693e0db38b1
Amitaibu <amitai@gizra.com>
2013-10-07 12:29:03 +0300

    More work.

--- import.sh
+++ import.sh
@@ -4,10 +4,12 @@ cd www
 drush sql-drop -y
 `drush sql-connect` < $1
 drush updb -y
+drush vrd -y --uri=$2
 drush dl acquia_connector
 drush en dblog -y
 drush en devel -y
 drush en os_apache_solr -y
+drush uli --uri=$2
 
 
 
